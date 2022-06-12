b833de9124e366140fb026dd2ab1e8564f1f6b59
Jean-Michel Bodart <jmbodart@skynet.be>
2022-06-12 21:24:26 +0200

    adapt to scan a few hosts. document need to run with sudo.

---
+++ Part_2/2.1 Network Scanning/PortScan.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+sudo /home/jmb/data/Pyproj/python-for-cybersecurity/.conda/PyCyberSec/bin/python /home/jmb/data/Pyproj/python-for-cybersecurity/Part_2/2.1\ Network\ Scanning/PortScan.py | tee /home/jmb/data/Pyproj/python-for-cybersecurity/Part_2/2.1\ Network\ Scanning/PortScan.out
+
